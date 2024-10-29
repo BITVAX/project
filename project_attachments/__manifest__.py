87da46524530113a1662e4c4f538fba3938d9f37
Ignacio J. Ortega <nacho.ortega@gmail.com>
2024-10-29 14:21:06 +0100

    [16.0][ADD] project_attachments: Recovers a functionality of previous version, adding a button to get all attached docs associated with a project and his tasks

---
+++ project_attachments/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Project attachments',
+    'version': '16.0.0.0.0',
+    'summary': 'Adds a button to project view to visualize the projects and project\'s task attachments',
+    'description': 'Adds a button to project view to visualize the projects and project\'s task attachments',
+    'category': 'Project',
+    'author': 'TRIVAX SL',
+    'website': 'http://www.3vax.com',
+    'license': 'AGPL-3',
+    'depends': ['project',],
+    'data': [
+        'views/project.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False
+}
